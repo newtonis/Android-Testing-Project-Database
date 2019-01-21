fdede5e5140b6b597539acd0553953d9de747274
newtonis <newtonis.penguin@gmail.com>
2019-01-20 23:03:03 -0300

    add

--- CalificaProfesores/app/src/main/java/com/gnd/calificaprofesores/ActivitySearchCourse.java
+++ CalificaProfesores/app/src/main/java/com/gnd/calificaprofesores/ActivitySearchCourse.java
@@ -1,5 +1,6 @@
 package com.gnd.calificaprofesores;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
@@ -8,11 +9,16 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.firebase.ui.database.SnapshotParser;
 import com.gnd.calificaprofesores.ListItems.BasicListItem;
+import com.gnd.calificaprofesores.ListItems.ListItemViewHolder;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -63,15 +69,49 @@ public class ActivitySearchCourse extends AppCompatActivity {
     public void firebaseClassSearch(String searchText){
         Query firebaseSearchQuery = mUserDatabase.orderByChild("Name").startAt(searchText).endAt(searchText + "\uf8ff");
 
-        /*FirebaseRecyclerOptions<BasicListItem> options =
+        FirebaseRecyclerOptions<BasicListItem> options =
                 new FirebaseRecyclerOptions.Builder<BasicListItem>()
                         .setQuery(firebaseSearchQuery, new SnapshotParser<BasicListItem>() {
                             @NonNull
                             @Override
                             public BasicListItem parseSnapshot(@NonNull DataSnapshot snapshot) {
-                                String details = "";
+                                String facultad = (String)snapshot.child("Facultad").getValue();
 
+
+                                return new BasicListItem((String)snapshot.child("Name").getValue(), facultad,Long.parseLong(snapshot.getKey()));
                             }
-                        });*/
+                        }).build();
+
+        FirebaseRecyclerAdapter adapter = new FirebaseRecyclerAdapter<BasicListItem,ListItemViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(ListItemViewHolder holder, int position, BasicListItem model) {
+                holder.setDetails(getApplicationContext(), model.getName(), model.getDetail());
+                /*final String professorName = model.getName();
+                final Long professorId = model.getId();*/
+
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        /*Intent intent = new Intent(ActivityProfessorList.this, ActivityProfesorFrontPage.class);
+                        intent.putExtra("ProfessorName",professorName);
+                        intent.putExtra("ProfessorId",professorId);
+
+                        startActivity(intent);*/
+                    }
+                });
+
+            }
+            @Override
+            public ListItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.search_list_element, parent, false);
+
+                return new ListItemViewHolder(view);
+            }
+        };
+
+        mResultList.setAdapter(adapter);
+        adapter.startListening();
+
     }
 }
