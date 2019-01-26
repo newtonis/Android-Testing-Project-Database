c5bf8bc3db26630fb4e390f56c359d7238483610
Ariel Nowik <newtonis.penguin@gmail.com>
2019-01-26 13:43:12 -0800

    purificando login

--- CalificaProfesores/app/src/main/java/com/gnd/calificaprofesores/ActivityUser.java
+++ CalificaProfesores/app/src/main/java/com/gnd/calificaprofesores/ActivityUser.java
@@ -1,16 +1,26 @@
 package com.gnd.calificaprofesores;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+
 public class ActivityUser extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    public static GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +28,20 @@ public class ActivityUser extends AppCompatActivity {
         setContentView(R.layout.activity_user);
 
 
+
+        mAuth = FirebaseAuth.getInstance();
+
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        /*mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();*/
 
         TextView welcomeText = findViewById(R.id.welcomeText);
 
@@ -50,5 +73,19 @@ public class ActivityUser extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        /*** Logout function ***/
+        Button exitButton = findViewById(R.id.ButtonExit);
+
+        exitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mAuth.getInstance().signOut();
+                Intent intent = new Intent(ActivityUser.this, ActivityLogin.class);
+                startActivity(intent);
+            }
+        });
+        /*** end logout function ***/
+
     }
 }
